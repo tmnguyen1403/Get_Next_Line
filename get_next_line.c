8d75d1f6ad89ef79146e012a399b31965fe49a2b
Tom Nguyen <tomriddle1403@gmail.com>
2019-09-25 19:21:40 -0500

    Working on

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,64 @@
 #include "get_next_line.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+//need to use ft_strncpy
+//ft_strnew
+//ft_strdel
+
+static ssize_t	find_newline(const char *s)
+{
+		ssize_t	index;
+
+		index = 0;
+		while (s[index])
+		{
+			if (s[index] == '\n')
+				return (index);
+			++index;
+		}
+		return (-1);
+}
 
 int get_next_line(const int fd, char **line)
 {
-	
+	ssize_t		ret;
+	char			s[BUFF_SIZE];
+	ssize_t		counter;
+	size_t		length;
+	char			*n_line;
+
+	line = 0;
+	ret = read(fd, s, BUFF_SIZE);
+	if (ret <= 0)
+		return (ret); // end of file
+	counter = find_newline((const char*)s);
+	if (counter == -1)
+		return (-1);
+		//save current string
+		//continue reading
+	if (counter > 0)
+	{
+		length = (size_t)counter;
+		n_line = ft_strnew(length + 1);
+		ft_strncpy(n_line, (const char *)s, length);
+		return (n_line);
+	}
+			//malloc new string, size = counter
+			//*line = new_string
+			//return 1;
+	if (counter == 0)
+		return (0);
+	return (1);
+}
+
+int main(int ac, char **av)
+{
+	char s[BUFF_SIZE];
+
+	ac = 0;
+	av = 0;
+	printf("%zd\n", read(1, s, BUFF_SIZE));
+	return (1);
 }
